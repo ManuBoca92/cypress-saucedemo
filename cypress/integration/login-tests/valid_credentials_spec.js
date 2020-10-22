c07e3d5a33209b5d86eee388cd2b4a58f5d14a08
Ebonom Mfam <ebonom.mfam@snow.dog>
2020-10-22 11:30:52 +0100

    added valid credentials login test

---
+++ cypress/integration/login-tests/valid_credentials_spec.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+describe("User login tests with valid credentials", () => {
+  it("Visits swag labs website.", () => {
+    cy.visit("/index.html");
+  });
+
+  it("Types in users username.", () => {
+    cy.get('[data-test="username"]').type("standard_user");
+  });
+
+  it("Types in users password.", () => {
+    cy.get('[data-test="password"]').type("secret_sauce");
+  });
+
+  it("Clicks on login button.", () => {
+    cy.get("#login-button").click();
+  });
+
+  it("Checks user is logged in and is on inventory page.", () => {
+    cy.url().should("includes", "/inventory.html");
+  });
+});
