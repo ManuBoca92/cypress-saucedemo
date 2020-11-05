d2439f5fc4a1f1473e325de939de5e839a7b74c4
Justyna Łęska <emmanuelmfam@gmail.com>
2020-11-05 17:34:05 +0000

    added test case for valid username login with invalid password

--- cypress/integration/login-tests/invalid_credentials_spec.js
+++ cypress/integration/login-tests/invalid_credentials_spec.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-describe("User login tests with invalid credentials", () => {
+describe("User login tests with invalid username and valid password", () => {
   it("Visits swag labs website.", () => {
     cy.visit("/index.html");
   });
@@ -11,6 +11,28 @@ describe("User login tests with invalid credentials", () => {
 
   it("types in users password.", () => {
     cy.get('[data-test="password"]').type("secret_sauce");
+  });
+
+  it("clicks on login button.", () => {
+    cy.get("#login-button").click();
+  });
+
+  it("Checks error message is displayed.", () => {
+    cy.get('[data-test="error"]').should("be.visible");
+  });
+});
+
+describe("User login tests with valid username and invalid password", () => {
+  it("Visits swag labs website.", () => {
+    cy.visit("/index.html");
+  });
+
+  it("types in users username.", () => {
+    cy.get('[data-test="username"]').type("standard_user");
+  });
+
+  it("types in users password.", () => {
+    cy.get('[data-test="password"]').type("public_sauce");
   });
 
   it("clicks on login button.", () => {
